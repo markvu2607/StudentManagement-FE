72f9a07125051426009312292100a0ebe884636e
vuvanminh267nt <vuvanminh267nt@gmail.com>
2022-03-03 23:33:31 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,26 @@
 
 
-var sideBar=document.querySelector('.sidebar');
-const x = sideBar.style.width;
-console.log(document.getElementById('show'))
-document.getElementById('show').onclick=function(){
-    var showText=document.querySelectorAll('.hide-text');
-    for(let i=0;i<showText.length;i++){
-        if(showText[i].style.display === "inline"){
-            showText[i].style.display ="none"
-            sideBar.style.width='50px';
-        }
-        else{
-            showText[i].style.display = "inline";
-            sideBar.style.width=x;
-        }
-        // showText[i].classList.toggle('hide');
-    }
-     
+// var sideBar = document.querySelector('.sidebar');
+// const x = sideBar.style.width;
+// console.log(document.getElementById('show'))
+// document.getElementById('show').onclick = function () {
+//     var showText = document.querySelectorAll('.hide-text');
+//     for (let i = 0; i < showText.length; i++) {
+//         if (showText[i].style.display === "inline") {
+//             showText[i].style.display = "none"
+//             sideBar.style.width = '50px';
+//         }
+//         else {
+//             showText[i].style.display = "inline";
+//             sideBar.style.width = x;
+//         }
+//         // showText[i].classList.toggle('hide');
+//     }
+
+// }
+
+const onLogout = () => {
+    localStorage.removeItem("user")
+    window.location = "../login.html"
+    console.log("logout")
 }
