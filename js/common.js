b840e6e2430008e9c8924aedf4519fb3ef03c543
vuvanminh267nt <vuvanminh267nt@gmail.com>
2022-03-11 07:14:11 +0700

    quan ly sinh vien, khoa, mon hoc

--- js/common.js
+++ js/common.js
@@ -3,3 +3,17 @@ const onLogout = () => {
   window.location = "../login.html"
   console.log("logout")
 }
+
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
