59b61c5315b2c8aa3a68e05b8870b02ed0d6dfb5
longvu138 <tranlongvu138@gmail.com>
2022-03-12 01:37:36 +0700

    done quanly bang tin - giao dien quanlygiangvien

--- js/common.js
+++ js/common.js
@@ -1,19 +1,17 @@
 const onLogout = () => {
-  localStorage.removeItem("user")
-  window.location = "../login.html"
-  console.log("logout")
-}
+  localStorage.removeItem("user");
+  window.location = "../login.html";
+  console.log("logout");
+};
 
 function formatDate(date) {
   var d = new Date(date),
-    month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
     year = d.getFullYear();
 
-  if (month.length < 2)
-    month = '0' + month;
-  if (day.length < 2)
-    day = '0' + day;
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
 
-  return [year, month, day].join('-');
+  return [year, month, day].join("-");
 }
