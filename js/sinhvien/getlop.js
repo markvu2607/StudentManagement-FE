122325d1e5e5aef914f5149fc13bb8e7366494cc
Thành Kiên <thankkien@hotmail.com>
2022-03-26 22:42:35 +0700

    Update getlop.js

--- js/sinhvien/getlop.js
+++ js/sinhvien/getlop.js
@@ -39,22 +39,23 @@ const renderDiemdanh = (id) => {
       let html = "";
       for (i = 0; i < data.length; i++) {
         elm = data[i];
-        html += `
-        <tr>
+        html += `<tr>
         <td>${i + 1}</td>
-        <td>${formatDate(elm.thoiGianBd)}</td>
-        <td>${formatDate(elm.thoiGianKt)}</td>
-        <td>
-            <a style="margin-right: 5px;" type="button"">
-                <i class="fa-solid fa-eye"></i>
-            </a>
-        </td>
+        <td>${formatDatetime(elm.thoiGianBd)}</td>
+        <td>${formatDatetime(elm.thoiGianKt)}</td>
+        <td>${
+          elm.trangThai === "vang"
+            ? "Vắng mặt"
+            : elm.trangThai === "muon"
+            ? "Muộn"
+            : "Có Mặt"
+        }</td>
         <th>
-             <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="switchDiemDanh" onclick="updateDiemDanh(elm.idDiemDanh)">
-                </div>
-            </th>
-        </tr>
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="switchDiemDanh" onclick="updateDiemDanh(elm.idDiemDanh)">
+            </div>
+        </th>
+      </tr>
       `;
       }
       document.querySelectorAll("#listDiemDanh").forEach((elm) => {
