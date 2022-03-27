3fefc83c8da50d82e2b44b8684d0eb50c9b497d9
longvu138 <tranlongvu138@gmail.com>
2022-03-28 01:25:59 +0700

    fix diem ren luyen

--- js/diemrenluyen.js
+++ js/diemrenluyen.js
@@ -9,16 +9,23 @@ const renderDiemRenLuyen = () => {
     .then((data) => {
       let html = "";
       let i = 0;
-      data.forEach((elm) => {
-        html += `<tr>
-                    <th scope="row">${i++}</th>
-                    <td>${elm.idsv}</td>
-                    <td>${elm.tensv}</td>
-                    <td>${elm.tenKhoa}</td>
-                    <td>${elm.tenKyHoc}</td>
-                    <td>${elm.diem}</td>
-                </tr>`;
-      });
+      if (data.length === 0) {
+        alert("Không có kết quả");
+        document.querySelector("#inputName").value = "";
+        renderDiemRenLuyen();
+      } else {
+        data.forEach((elm) => {
+          html += `<tr>
+                      <th scope="row">${i++}</th>
+                     
+                      <td>${elm.idsv}</td>
+                      <td>${elm.tensv}</td>
+                      <td>${elm.tenKhoa}</td>
+                      <td>${elm.tenKyHoc}</td>
+                      <td>${elm.diem}</td>
+                  </tr>`;
+        });
+      }
 
       document.querySelector("#ListDiemRenLuyen").innerHTML = html;
     })
