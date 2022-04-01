d2061a56d25a21299f5d99b9d2f6a8b757068f4b
Thành Kiên <thankkien@hotmail.com>
2022-04-01 17:44:52 +0700

    new navbar animation

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,16 +1,32 @@
 var sideBar = document.querySelector(".sidebar");
 const x = sideBar.style.width;
-const menuOpenClose = function () {
+document.getElementById("menu").addEventListener("mouseover", function () {
   var showText = document.querySelectorAll(".ms-2");
   for (let i = 0; i < showText.length; i++) {
     if (showText[i].style.display == "none") {
       showText[i].style.display = "inline";
-      sideBar.style.width = "280px";
-    } else {
-      showText[i].style.display = "none";
-      sideBar.style.width = x;
     }
   }
-};
-// document.getElementById("menu").onclick = menuOpenClose;
-document.getElementById("show").onclick = menuOpenClose;
+  sideBar.style.width = "280px";
+});
+document.getElementById("menu").addEventListener("mouseout", function () {
+  var showText = document.querySelectorAll(".ms-2");
+  for (let i = 0; i < showText.length; i++) {
+    if (showText[i].style.display == "inline") {
+      showText[i].style.display = "none";
+    }
+  }
+  sideBar.style.width = x;
+});
+document.getElementById("show").addEventListener("click", function () {
+  var showText = document.querySelectorAll(".ms-2");
+  for (let i = 0; i < showText.length; i++) {
+    if (showText[i].style.display == "inline") {
+      showText[i].style.display = "none";
+      sideBar.style.width = x;
+    } else {
+      showText[i].style.display = "inline";
+      sideBar.style.width = "280px";
+    }
+  }
+});
