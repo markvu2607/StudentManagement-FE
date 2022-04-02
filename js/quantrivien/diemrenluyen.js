2f3f9f657ca6428ed3cc483e7ab8b5bb55c4b4ce
Thành Kiên <thankkien@hotmail.com>
2022-04-02 11:14:33 +0700

    ql drl

--- js/quantrivien/diemrenluyen.js
+++ js/quantrivien/diemrenluyen.js
@@ -2,17 +2,19 @@ const renderDiemRenLuyen = () => {
   const inputName = document.querySelector("#inputName").value.trim();
   const inputKhoa = document.querySelector("#khoa").value;
   const inputKyHoc = document.querySelector("#kyHoc").value;
-  fetch(
-    `${HOST}/api/diemrenluyen/?idKhoa=${inputKhoa}&idky=${inputKyHoc}&tensv=${inputName}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      let html = "";
-      let i = 0;
-      if (data.length > 0) {
-        for (i = 0; i < data.length; i++) {
-          elm = data[i];
-          html += `<tr>
+  if (!inputKhoa || !inputKyHoc) alert("Yêu cầu chọn đầy đủ thông tin");
+  else if (!inputName) alert("Yêu cầu nhập tên sinh viên");
+  else
+    fetch(
+      `${HOST}/api/diemrenluyen/?idKhoa=${inputKhoa}&idky=${inputKyHoc}&tensv=${inputName}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let html = "";
+        if (data.length > 0) {
+          for (i = 0; i < data.length; i++) {
+            elm = data[i];
+            html += `<tr>
                       <th scope="row">${i + 1}</th>
                       <td>${elm.idsv}</td>
                       <td>${elm.tensv}</td>
@@ -20,9 +22,9 @@ const renderDiemRenLuyen = () => {
                       <td>${elm.tenKyHoc}</td>
                       <td>${elm.diem}</td>
                   </tr>`;
-        }
-      } else alert("Không có kết quả");
-      document.querySelector("#ListDiemRenLuyen").innerHTML = html;
-    })
-    .catch((err) => console.log("Error: ", err));
+          }
+        } else alert("Không có kết quả");
+        document.querySelector("#ListDiemRenLuyen").innerHTML = html;
+      })
+      .catch((err) => console.log("Error: ", err));
 };
